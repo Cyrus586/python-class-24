5b5fdb78b707a4ae3e3c70f9c0b2d437021985cb
Cyrus586 <aksamcyrus@gmail.com>
2024-02-07 04:55:05 -0800

    Operators

--- assignments/asignment_7/divide.py
+++ assignments/asignment_7/divide.py
@@ -1,3 +1,15 @@
+def divided(num1,num2):
+    return (num1 / num2)
+
+def get_input():
+    number_1 = float(input('Enter Your First Number '))
+    number_2 = float(input('Enter Your Second Number '))
+    print("The divided values = ", divided(number_1, number_2))
+
+get_input()
+
+print()
+# EXAMPLE 2
 
 
 def divide(number_1, number_2):
--- assignments/asignment_7/subtract.py
+++ assignments/asignment_7/subtract.py
@@ -1,8 +1,7 @@
 # EXAMPLE 1
-
 def subract(number_1, number_2):
     number_1 = float(input('Enter Your First Number '))
-    number_2 = float(input('Enter Your Second Number ')   )
+    number_2 = float(input('Enter Your Second Number '))
     result = float(number_1) - float(number_2)
     return (result)
 
@@ -15,3 +14,17 @@ outPut()
 
 
 
+print()
+# EXAMPLE 2
+
+print('EXAMPLE 2')
+
+def subracted(num1,num2):
+    return (num1 - num2)
+
+def get_input():
+    number_1 = float(input('Enter Your First Number '))
+    number_2 = float(input('Enter Your Second Number '))
+    print("The subtracted values = ", subracted(number_1, number_2))
+
+get_input()
